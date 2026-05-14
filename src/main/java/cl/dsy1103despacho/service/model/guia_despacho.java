5f57386d46b3b11b572c8d35ba75f07340547899
Matias Tiznado <m.tiznado@duocuc.cl>
2026-05-14 09:09:35 -0400

    Model v1

---
+++ src/main/java/cl/dsy1103despacho/service/model/guia_despacho.java
@@ -0,0 +1,35 @@
+package cl.dsy1103despacho.service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "guia_despacho")
+public class guia_despacho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_guia")
+    private Long idGuia;
+
+    @Column(name = "id_pedido")
+    private Long idPedido;
+
+    @Column(name = "direccion_envio")
+    private String direccionEnvio;
+
+    @Column(name = "estado_envio")
+    private String estadoEnvio;
+
+    @Column(name = "fecha_estimada")
+    private LocalDateTime fechaEstimada;
+
+}
---
+++ src/test/java/cl/dsy1103despacho/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dsy1103despacho.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
